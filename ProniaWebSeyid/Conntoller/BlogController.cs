f3bd48a7a82f0836a19efb42df310df04e110517
Seyidakif101 <sayidakhifia-mpa101@code.edu.az>
2025-12-22 00:19:59 +0400

    Pronia

---
+++ ProniaWebSeyid/Conntoller/BlogController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProniaWebSeyid.Conntoller
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ProniaWebSeyid/Conntoller/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProniaWebSeyid;
+
+namespace ProniaWebSeyid.Conntoller
+{
+    public class HomeController : Controller
+    {
+        private readonly AddDbContext _context;
+        public HomeController(AddDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var shippings = _context.Shippings.ToList();
+            return View(shippings);
+        }
+
+    }
+}
